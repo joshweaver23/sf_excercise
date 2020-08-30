48a0d83e9429fbb63469d3c97d4fa7a226e6ae67
Josh <jweaver@snapcap.com>
2020-08-30 15:28:45 -0400

    added some frontend client to display data returned to server

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = {
+      fireIncidents: [],
+      policeIncidents: [],
+    };
   }
 
   callAPI() {
     fetch('http://localhost:9000/data')
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }))
+      .then(res => res.json())
+      .then(res => this.setState({
+        fireIncidents: res.fireIncidents,
+        policeIncidents: res.policeIncidents,
+      }))
       .catch(err => console.log(err));
   };
 
@@ -22,8 +28,45 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>{this.state.apiResponse}</p>
+          <h4>Most Recent Chapel Hill Fire and Police Calls</h4>
         </header>
+        <section id='dataDisplay' className='main-section'>
+          {this.state.fireIncidents.length > 0 ? (
+            <div className='calls-box'>
+              <h4>Fire Calls</h4>
+              <ul>
+                {this.state.fireIncidents.map((incident, index) => {
+                  return (
+                    <li key={`${incident}-${index}`}>{incident}</li>
+                  )
+                })}
+              </ul>
+            </div>
+          ) : (
+              <div className='calls-box'>
+                <h4>Fire Calls</h4>
+                <p>Loading ...</p>
+              </div>
+            )}
+
+          {this.state.policeIncidents.length > 0 ? (
+            <div className='calls-box'>
+              <h4>Police Calls</h4>
+              <ul>
+                {this.state.policeIncidents.map((incident, index) => {
+                  return (
+                    <li key={index}>Call: {incident.reportedAs} || Report: {incident.loggedAs}</li>
+                  )
+                })}
+              </ul>
+            </div>
+          ) : (
+              <div className='calls-box'>
+                <h4>Police Calls</h4>
+                <p>Loading ...</p>
+              </div>
+            )}
+        </section>
       </div>
     );
   }
